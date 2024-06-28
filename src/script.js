efa3864f39f380a4991bc22d64107d472d234cea
Roberto Aleman <165515200+roberto-aleman@users.noreply.github.com>
2024-06-28 16:29:29 -0700

    feat: add square grid to be used as etch-a-sketch to homepage

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+const container = document.querySelector("#container");
+let squaresPerSide = 16;
+for (let i = 0; i < squaresPerSide; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < squaresPerSide; j++) {
+        const div = document.createElement("div");
+        div.setAttribute("class", "square");
+        row.appendChild(div);
+    }
+    container?.appendChild(row);
+}
 export {};
