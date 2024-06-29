076cd41608470460cf243770d98d9ba8d848030b
Roberto Aleman <165515200+roberto-aleman@users.noreply.github.com>
2024-06-28 17:40:39 -0700

    feat: change background color of square when hovered

--- src/script.ts
+++ src/script.ts
@@ -5,9 +5,17 @@ for (let i = 0; i < squaresPerSide; i++) {
   const row = document.createElement("div");
   row.setAttribute("class", "row");
   for (let j = 0; j < squaresPerSide; j++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "square");
-    row.appendChild(div);
+    row.appendChild(createSquare());
   }
   container?.appendChild(row);
+}
+
+function createSquare() {
+  const square = document.createElement("div");
+  square.setAttribute("class", "square");
+  square.addEventListener("mouseover", (e) => {
+    const target = e.currentTarget as HTMLDivElement;
+    target.style.backgroundColor = "black";
+  });
+  return square;
 }
